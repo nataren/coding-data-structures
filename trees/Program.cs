e16f825d1d7b52f45ca1464a60c536977bf45f40
César López-Natarén <cesar@nataren.io>
2017-07-12 16:51:21 -0700

    Implement In Order, Pre Order, and Post Order tree traversals

--- trees/Program.cs
+++ trees/Program.cs
@@ -1,62 +1,74 @@
 ﻿using System;
-
-public class Node {
-    public int Value;
-    public Node[] Children;
-
-    public Node(int value, Node[] children) {
-        this.Value = value;
-        this.Children = children;
-    }
-}
+using System.Linq;
+using System.Collections.Generic;
 
 public class BinaryTreeNode<T> {
     public T Value;
     public BinaryTreeNode<T> Left;
     public BinaryTreeNode<T> Right;
 
-    public BinaryTreeNode(T value, BinaryTreeNode<T> left, BinaryTreeNode<T> right) {
+    public BinaryTreeNode(T value, BinaryTreeNode<T> left = null, BinaryTreeNode<T> right = null) {
         this.Value = value;
         this.Left = left;
         this.Right = right;
     }
 
-    public void InOrderTraversal() {
+    public IEnumerable<T> InOrderTraversal() {
         if(Left != null) {
-            Left.InOrderTraversal();
+            foreach(var node in Left.InOrderTraversal()) {
+                yield return node;
+            }
         }
-        Visit();
+        yield return Visit();
         if(Right != null) {
-            Right.InOrderTraversal();
+            foreach(var node in Right.InOrderTraversal()) {
+                yield return node;
+            }
         }
     }
 
-    private void Visit() {
-        Console.WriteLine(Value);
+    public IEnumerable<T> PreOrderTraversal() {
+        yield return Visit();
+        if(Left != null) {
+            foreach(var node in  Left.PreOrderTraversal()) {
+                yield return node;
+            }
+        }
+        if(Right != null) {
+            foreach(var node in Right.PreOrderTraversal()) {
+                yield return node;
+            }
+        }
+    }
+
+    public IEnumerable<T> PostOrderTraversal() {
+        if(Left != null) {
+            foreach(var node in Left.PostOrderTraversal()) {
+                yield return node;
+            }
+        }
+        if(Right != null) {
+            foreach(var node in Right.PostOrderTraversal()) {
+                yield return node;
+            }
+        }
+        yield return Visit();
+    }
+
+    private T Visit() {
+        return Value;
     }
 }
 
 static class Program {
     static void Main(string[] args) {
-        var tree = new Node(10,
-                            new Node[] {
-                                new Node(5,
-                                         new Node[] {
-                                             new Node(3, null),
-                                             new Node(6, null)
-                                         }),
-                                new Node(15,
-                                         new Node[] {
-                                             new Node(11, null),
-                                             new Node(16, null)
-                                         })
-                            });
+        var bt = new BinaryTreeNode<int>(10,
+                                         new BinaryTreeNode<int>(5, new BinaryTreeNode<int>(3), new BinaryTreeNode<int>(6)),
+                                         new BinaryTreeNode<int>(15, new BinaryTreeNode<int>(11), new BinaryTreeNode<int>(16, null, new BinaryTreeNode<int>(17))));
 
-        var bt = new BinaryTreeNode<int>(
-                                                 10,
-                                                 new BinaryTreeNode<int>(5, new BinaryTreeNode<int>(3, null, null), new BinaryTreeNode<int>(6, null, null)),
-                                                 new BinaryTreeNode<int>(15, new BinaryTreeNode<int>(11, null, null), new BinaryTreeNode<int>(16, null, new BinaryTreeNode<int>(17, null, null))));
-        bt.InOrderTraversal();
+        Console.WriteLine("InOrder=[{0}]", string.Join(", ", bt.InOrderTraversal().ToArray()));
+        Console.WriteLine("PreOrder=[{0}]", string.Join(", ", bt.PreOrderTraversal().ToArray()));
+        Console.WriteLine("PostOrder=[{0}]", string.Join(", ", bt.PostOrderTraversal().ToArray()));
     }
 }
 
