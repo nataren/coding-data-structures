3b6e43b7ca8191923dc2add7456d1e5b41eda705
César López-Natarén <cesar@nataren.io>
2017-07-17 23:08:12 -0700

    Implement check_permutations solution

--- arrays/solutions.py
+++ arrays/solutions.py
@@ -4,3 +4,25 @@ def contains_duplicates(str):
             if str[i] == j:
                 return True
     return False
+
+def check_permutation(s1, s2):
+    if len(s1) != len(s2):
+        return False
+
+    s1_counts = [None] * 128
+    for (c, x) in [(c, 1) for c in s1]:
+        i = ord(c)
+        if s1_counts[i] == None:
+            s1_counts[i] = 1
+        else:
+            s1_counts[i] += 1
+
+    s2_counts = [None] * 128
+    for (c, x) in [(c, 1) for c in s2]:
+        i = ord(c)
+        if s2_counts[i] == None:
+            s2_counts[i] = 1
+        else:
+            s2_counts[i] += 1
+    return s1_counts == s2_counts
+
--- arrays/test_solutions.py
+++ arrays/test_solutions.py
@@ -1,8 +1,9 @@
 import pytest
 
 from solutions import contains_duplicates
+from solutions import check_permutation
 
-class TestArraySolutions(object):
+class TestContainsDuplicatesSolution(object):
     def test_does_not_contains_on_empty_string(self):
         assert not contains_duplicates("")
 
@@ -20,3 +21,27 @@ class TestArraySolutions(object):
 
     def test_contains_duplicates_in_the_middle(self):
         assert contains_duplicates("abcdefgaxyz")
+
+
+class TestCheckPermutation(object):
+    def test_empty_strings(self):
+        assert check_permutation('', '')
+
+    def test_single_character_permutation(self):
+        assert check_permutation('a', 'a')
+
+    def test_single_character_different(self):
+        assert not check_permutation('a', 'b')
+
+    def test_multiple_characters_are_not_permutation(self):
+        assert not check_permutation('aabbc', 'azzzz')
+
+    def test_different_length_strings(self):
+        assert not check_permutation('asdfasdfasdf', 'ab')
+
+    def test_multiple_characters_permutation(self):
+        assert check_permutation('abcd', 'dcba')
+
+
+
+
