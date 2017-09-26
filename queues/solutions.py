a241bddfbcfadd9e7d0e2870518ddcbbe7136f9e
César López-Natarén <cesar@nataren.io>
2017-09-26 09:25:59 -0700

    Implement queue

--- queues/solutions.py
+++ queues/solutions.py
@@ -1,21 +1,36 @@
 class Queue:
     class Node:
-        def __init__(self, data, next):
+        def __init__(self, data):
             self.data = data
-            self.next = next
+            self.next = None
 
     def __init__(self):
         self.first = None
         self.last = None
 
     def add(self, item):
-        pass
+        new_node = Node(item)
+        if self.last:
+            self.last.next = new_node
+        self.last = new_node
 
     def remove(self):
-        pass
+        if self.first is None:
+            raise Exception('Queue is empty')
+
+        current_first = self.first
+        current_data = current_first.data
+        self.first = current_first.next
+
+        if self.first is None:
+            self.last = None
+
+        return current_data
 
     def peek(self):
-        pass
+        if self.first:
+            return self.first.data
+        raise Exception('No elements to peek')
 
     def is_empty(self):
         return self.first is None
